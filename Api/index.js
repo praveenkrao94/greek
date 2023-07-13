212131df9922c8db989f0ff960c17a86f85ad78e
praveenkrao94 <praveenkr0812@gmail.com>
2023-07-13 11:41:08 +0530

    PATCH ADDED

--- Api/index.js
+++ Api/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-  methods: 'GET, POST, PUT, DELETE',
+  methods: 'GET, POST, PUT, DELETE, PATCH',
 };
 
 
